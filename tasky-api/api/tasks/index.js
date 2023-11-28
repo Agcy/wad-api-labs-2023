e2bac00a77ccad955a95237beb8a5404292ec6bf
Haopeng Liang <765407782@qq.com>
2023-11-28 14:26:12 +0000

    Mongo lab Error Handling

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Task from './taskModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -14,4 +15,11 @@ router.post('/', async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
 });
+
+// create a task
+router.post('/', asyncHandler(async (req, res) => {
+    const task = await Task(req.body).save();
+    res.status(201).json(task);
+}));
+
 export default router;
