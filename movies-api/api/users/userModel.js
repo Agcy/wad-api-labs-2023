6441074c2b690a63fa4def6c357a0777e96f34a0
Haopeng Liang <765407782@qq.com>
2023-12-05 13:51:56 +0000

    Added Movies Route

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import TaskSchema from "../tasks/taskModel";
 
 const Schema = mongoose.Schema;
 
--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
+import moviesRouter from './api/movies';   //import movies router
 import './db';
 import defaultErrHandler from './errHandler'
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
+app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
 
 app.listen(port, () => {
     console.info(`Server running at ${port}`);
